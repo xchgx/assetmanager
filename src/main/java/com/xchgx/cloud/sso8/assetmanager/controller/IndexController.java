2a418e6867707a1e0136d46b273f45c7b6697922
xchgx <xchgx@163.com>
2020-03-27 11:02:45 +0800

    第05课 资产出库课堂笔记，出库参数2个，入库单号和使用者

--- src/main/java/com/xchgx/cloud/sso8/assetmanager/controller/IndexController.java
+++ src/main/java/com/xchgx/cloud/sso8/assetmanager/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.xchgx.cloud.sso8.assetmanager.controller;
 
 import com.xchgx.cloud.sso8.assetmanager.domain.Asset;
+import com.xchgx.cloud.sso8.assetmanager.domain.AssetRukudan;
 import com.xchgx.cloud.sso8.assetmanager.repository.AssetRepository;
+import com.xchgx.cloud.sso8.assetmanager.repository.AssetRukudanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +15,9 @@ import java.util.List;
 public class IndexController {//首页控制器
     //导入数据库持久化对象
     @Autowired //自动注入，理解为自动导入类对象
-    private AssetRepository assetRepository;
+    private AssetRukudanRepository assetRukudanRepository;
+    @Autowired
+    private AssetRepository assetRepository;//自动注入 资产持久化类
 
     //相当于我们之前分析资产管理系统时候，出现的 网页界面类
     @GetMapping({"/index","/"})//设置访问url网址
@@ -21,17 +25,42 @@ public class IndexController {//首页控制器
         return "大家好，欢迎来到Spring Boot项目现场，我是首页index"; //直接返回一句话（字符串）
     }
 
-    @GetMapping("/add") //添加资产，资产入库
-    public Asset add(@RequestParam String name,@RequestParam int number){ //接收前端传送过来的参数
-        Asset asset = new Asset(); //创建一个资产对象
-        asset.setName(name); //设置资产名称-来自参数 name
-        asset.setNumber(number);  //设置资产数量-来自参数 number
-        Asset resultAsset = assetRepository.save(asset);//保存资产对象到数据库中，然后返回存储后的资产对象
-        return resultAsset; //返回存储后的资产对象
+    @GetMapping("/add") //添加资产入库单，资产入库单
+    public AssetRukudan add(@RequestParam String name, @RequestParam int amount){ //接收前端传送过来的参数
+        AssetRukudan assetRukudan = new AssetRukudan(); //创建一个资产对象
+        assetRukudan.setName(name); //设置资产名称-来自参数 name
+        assetRukudan.setAmount(amount);  //设置资产数量-来自参数 number
+        AssetRukudan resultAssetRukudan = assetRukudanRepository.save(assetRukudan);//保存资产对象到数据库中，然后返回存储后的资产对象
+        return resultAssetRukudan; //返回存储后的资产对象
     }
 
     @GetMapping("/list") //访问网址是 http://localhost:8080/list
-    public List<Asset> list(){//列出所有资产
-        return assetRepository.findAll();//查询所有资产
+    public List<AssetRukudan> list(){//列出所有资产
+        return assetRukudanRepository.findAll();//查询所有资产
     }
+
+
+    /**
+     * //    资产出库，你要什么资产？电脑、投影仪、笔记本、手机、桌子
+     * @param rukudan 入库单主键ID
+     * @param username 使用者用户名，唯一特性
+     * @return 保存后的资产对象
+     */
+
+    @GetMapping("/chuku") //接口地址，访问的网址
+    public Asset chuku(long rukudan,String username){ //出库的方法
+        //出库的内容来自于入库单
+        //通过入库单ID查询入库单记录，如果找不到就返回null
+        AssetRukudan assetRukudan=assetRukudanRepository.findById(rukudan).orElse(null);
+
+        Asset asset = new Asset();//创建资产对象
+        asset.setName(assetRukudan.getName());//设置资产名称
+        asset.setPrice(assetRukudan.getPrice());//设置单价
+        asset.setRukudanId(assetRukudan.getId());//设置我们的资产来自于哪一个入库单ID
+        asset.setUsername(username);//设置使用者
+        Asset save = assetRepository.save(asset);//把资产保存到数据库中
+        return save;//返回保存后的资产
+    }
+
+
 }
--- src/main/java/com/xchgx/cloud/sso8/assetmanager/domain/AssetRukudan.java
+++ src/main/java/com/xchgx/cloud/sso8/assetmanager/domain/AssetRukudan.java
@@ -1,26 +1,30 @@
 package com.xchgx.cloud.sso8.assetmanager.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Date;
-@Entity
-@Data
-public class Asset { //资产类
+
+/**
+ * 资产类，域对象
+ */
+@Entity //创建数据表相关代码 在类上添加注解，说明该类是数据表
+@Getter //自动添加get方法
+@Setter //自动添加set方法
+public class AssetRukudan { //资产类
     @Id //创建数据表相关代码  主键
     @GeneratedValue //创建数据表相关代码  自动递增
     private long id; //数据表的主键
-//    private int amount;//资产数量 //单个资产不需要数量
+    private int amount;//资产数量
     private String name;//资产名称
     private String type;//资产类型
     private String readme;//资产说明书
     private float price;//资产单价
-    private String status;//资产状态 // 入库单没有资产状态
+//    private String status;//资产状态 // 入库单没有资产状态
     private Date scrq;//生产日期
     private String bzq;//保质期
     private Date rukuDate;//入库日期，相当于采购批次
-    private long rukudanId;//保存入库单主键ID
-    private String username;//用户名 谁在使用我们的资产 资产使用者
 }
