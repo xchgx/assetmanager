8cfb62ac06ef086fcd4698b96b7c15b83497ae0f
xchgx <xchgx@163.com>
2020-04-14 01:15:26 +0800

    已经不知道是什么版本了，操作了一下合并的结果就奔溃了。。 从v15beike合并到product3.0 可能操作有问题，导致现在这个情况，先提交一下，再试试。

--- src/main/java/com/xchgx/cloud/sso8/assetmanager/repository/AssetRukudanRepository.java
+++ src/main/java/com/xchgx/cloud/sso8/assetmanager/repository/AssetRukudanRepository.java
@@ -1,11 +1,11 @@
 package com.xchgx.cloud.sso8.assetmanager.repository;
 
-import com.xchgx.cloud.sso8.assetmanager.domain.AssetRukudan;
+import com.xchgx.cloud.sso8.assetmanager.domain.AssetRuKuDan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * 创建 资产入库单持久化接口
+ * 创建 资产持久化接口
  * 只要继承了JpaRepository，我们就自动拥有了添加，修改，删除的功能
  */
-public interface AssetRukudanRepository extends JpaRepository<AssetRukudan,Long> {//继承数据库的标准操作功能
+public interface AssetRuKuDanRepository extends JpaRepository<AssetRuKuDan,Long> {//继承数据库的标准操作功能
 }
