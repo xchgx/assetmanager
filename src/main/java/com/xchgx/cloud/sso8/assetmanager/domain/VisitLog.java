7fee93f2a1e1fab15c6f63715005cf4f0e0f2e5a
xchgx <xchgx@163.com>
2020-04-25 00:46:29 +0800

    第24课 实战-日志子系统 备课

---
+++ src/main/java/com/xchgx/cloud/sso8/assetmanager/domain/VisitLog.java
@@ -0,0 +1,38 @@
+package com.xchgx.cloud.sso8.assetmanager.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class VisitLog {
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @NonNull
+    private String url;//访问地址
+    @NonNull
+    private String ip;//客户端IP
+    @NonNull
+    private String browser;//浏览器信息
+    @NonNull
+    private String username;//用户名
+    @NonNull
+    private String name;//姓名
+    @NonNull
+    private String query;//查询字符串
+    @NonNull
+    private String method;//访问方式
+    @NonNull
+    private String os;//客户端端口
+    private Date date = new Date();//浏览器信息
+}
--- src/main/java/com/xchgx/cloud/sso8/assetmanager/filter/A0VisitLogFilter.java
+++ src/main/java/com/xchgx/cloud/sso8/assetmanager/filter/A0VisitLogFilter.java
@@ -1,40 +1,69 @@
 package com.xchgx.cloud.sso8.assetmanager.filter;
 
 import com.xchgx.cloud.sso8.assetmanager.domain.User;
+import com.xchgx.cloud.sso8.assetmanager.domain.VisitLog;
+import com.xchgx.cloud.sso8.assetmanager.repository.VisitLogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
 @WebFilter(filterName = "logFilter",urlPatterns = "/*")
 public class A0VisitLogFilter implements Filter {
+    @Autowired
+    VisitLogRepository visitLogRepository;
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        User user = (User) request.getSession().getAttribute("user");
+        System.out.println("A0LogFilter.doFilter");
         StringBuffer requestURL = request.getRequestURL();
         System.out.println("requestURL = " + requestURL);
-
+        String requestURI = request.getRequestURI();
+        System.out.println("requestURI = " + requestURI);
         String queryString = request.getQueryString();
         System.out.println("queryString = " + queryString);
 
+        String method = request.getMethod();
+        System.out.println("method = " + method);
         String remoteAddr = request.getRemoteAddr();
         System.out.println("remoteAddr = " + remoteAddr);
-
         int remotePort = request.getRemotePort();
         System.out.println("remotePort = " + remotePort);
-
         String header = request.getHeader("user-agent");
         System.out.println("header = " + header);
-
-        User user = (User) request.getSession().getAttribute("user");
-        if(user==null){
-            System.out.println("无人登录 ");
+        String os = "windows";
+        if (header.toLowerCase().contains("windows")){
+            os = "windows";
+        }else if(header.toLowerCase().contains("linux")){
+            os = "linux";
+        }else if(header.toLowerCase().contains("iphone")){
+            os = "ios";
+        }else if(header.toLowerCase().contains("android")){
+            os = "android";
         }else{
-            System.out.println("user = " + user);
+            os = "other os";
         }
-        System.out.println("new Date().toLocaleString() = " + new Date().toLocaleString());
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        if (user == null) {
+            VisitLog log = new VisitLog(requestURL.toString(), remoteAddr, header, "未登录", "无名氏",queryString==null?"":queryString,method,os);
+            visitLogRepository.save(log);
+        }else{
+            VisitLog log = new VisitLog(requestURL.toString(), remoteAddr, header, user.getUsername(), user.getName(),queryString==null?"":queryString,method,os);
+            visitLogRepository.save(log);
+        }
+
+//        if(user == null){
+//            System.out.println("user = " + user);
+//            response.sendRedirect("/login/");//进入到登录页
+//            return;
+//        }
+        System.out.println(user);
+        filterChain.doFilter(servletRequest, servletResponse);//放行
     }
 }
